7744a96f6c663f3e767206b8a52959ae0f80c496
Raymond Huang <60895775+RaymondHuang44@users.noreply.github.com>
2025-05-05 04:50:49 -0700

    updated look for results page and added buttons for meal plan

--- food-rec-frontend/src/pages/ResultsPage.js
+++ food-rec-frontend/src/pages/ResultsPage.js
@@ -3,24 +3,24 @@ import Recommendations from '../components/Recommendations';
 import RecipeCard from '../components/RecipeCard';
 import '../App.css';
 
-const ResultsPage = ({ filters, results, onSearch }) => {
+const ResultsPage = ({ filters, results, onSearch, onGetMealPlan }) => {
   return (
-    <div className="container">
-      <div className="header">
-        CraveWise
-      </div>
-      <div className="results-container">
-        <div className="filters-panel">
-          <Recommendations
-            onSearch={onSearch}
-            initialMaxCalories={filters.maxCalories}
-            initialAllergies={filters.allergies}
-            initialProtein={filters.protein}
-          />
-        </div>
-        <div className="results-panel">
-          <div style={{ fontWeight: 'bold', fontSize: '2rem', marginBottom: '16px', width: '100%' }}>Results</div>
-          <div className="results-list" style={{ width: '100%' }}>
+    <div className="results-bg">
+      <div className="results-header-container">
+        <div className="header results-header">CraveWise</div>
+        <div className="results-floating-island">
+          <div className="results-header-row">
+            <span className="results-title">Results</span>
+            <span>
+              <button className="material-button" style={{ marginRight: 8, position: 'relative', left: '140px' }} onClick={() => onGetMealPlan(1)}>
+                Get 1-Day Meal Plan
+              </button>
+              <button className="material-button" style= {{position: 'relative', left: '150px'}} onClick={() => onGetMealPlan(7)}>
+                Get 7-Day Meal Plan
+              </button>
+            </span>
+          </div>
+          <div className="results-list">
             {results.length === 0 ? (
               <div>No results found.</div>
             ) : (
